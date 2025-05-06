bf09bfb60f5a0d3fc0342c174152847f87e63917
Taku-pg <kaitsu505@gmail.com>
2025-05-06 07:54:04 +0200

    with minor change for Client class

--- Model/Client.cs
+++ Model/Client.cs
@@ -5,11 +5,14 @@ namespace apbd_tutorial8.Model;
 public class Client
 {
     public int IdClient { get; set; }
+    [MaxLength(120)]
     public string FirstName { get; set; }
+    [MaxLength(120)]
     public string LastName { get; set; }
     [EmailAddress]
     public string Email { get; set; }
     [Phone]
     public string Telephone { get; set; }
+    [MinLength(120)]
     public string Pesel { get; set; }
 }
